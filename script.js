9b41ebd08d15f13ccb2c067206cd774e0e4ae81c
Adebola Odubiyi <Adebola.Odubiyi@Sterling.ng>
2022-04-29 16:42:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//TURN ON
+
+    var btn = document.getElementById("on-button");
+
+    btn.onclick = function(btn){
+        document.getElementById("myImage") = "img/bulb on.gif"
+       
+    }
+    
